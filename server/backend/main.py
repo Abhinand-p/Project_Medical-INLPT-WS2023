039e21d764f8f7d1f3f1a266792eb75bb38c3f35
john-hildenbrand@gmx.de <john-hildenbrand@gmx.de>
2023-11-21 01:01:06 +0100

    [John] added documentation, restored backend/main file

--- server/backend/main.py
+++ server/backend/main.py
@@ -1,6 +1,25 @@
+from typing import Union
 from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
+from fastapi import Body
+from fastapi.middleware.cors import CORSMiddleware
+#from transformers import pipeline
 
-async def hello():
-    return {"message": "good evenung"}
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/convert")
+def convert_text(text: str = Body(..., embed=True)):
+    return {text.lower()}
