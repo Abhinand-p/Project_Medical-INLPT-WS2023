0b2da95cbadc5bb25d894060ae819418ff2abf10
Hussein Abdulreda <hrida@192.168.82.250>
2024-02-28 13:01:33 +0100

    [Hussein] Adding prompts constant holder for manipulating between prompts

---
+++ server/backend/utils/prompts_constants.py
@@ -0,0 +1,8 @@
+###### Prompts ######
+prompt_01 = "Query: "
+prompt_02 = "Contexts: "
+prompt_03 = "QUESTION: "
+prompt_04 = "ANSWER: "
+prompt_10 = "Answer the question with the given Context:"
+prompt_11 = "Answer the question with the given Contexts. If the contexts can't answer the question explicitly say so and answer the question with your own knowledge.:"
+prompt_12 = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). if you don't know the answer, just say that you don't know, don't try to make up an answer. ALWAYS return a "SOURCES" part in your answer.\n"""
